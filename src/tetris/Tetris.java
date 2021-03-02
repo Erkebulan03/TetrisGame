811649e62dfa117bedbad656943c31a59531697c
Erkebulan03 <erkebulanamanbaj991@gmail.com>
2021-03-02 11:27:55 +0600

    BoardTetris

---
+++ src/tetris/Tetris.java
@@ -0,0 +1,31 @@
+package tetris;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Tetris extends JFrame {
+
+    private JLabel statusBar;
+
+    public Tetris() {
+        statusBar = new JLabel("0");
+        add(statusBar, BorderLayout.SOUTH);
+        Scoreboard board = new Scoreboard(this);
+        add(board);
+        board.start();
+        setSize(200, 400);
+        setTitle("My Tetris");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public JLabel getStatusBar() {
+        return statusBar;
+    }
+
+    public static void main(String[] args) {
+        Tetris myTetris = new Tetris();
+        myTetris.setLocationRelativeTo(null);
+        myTetris.setVisible(true);
+    }
+
+}
